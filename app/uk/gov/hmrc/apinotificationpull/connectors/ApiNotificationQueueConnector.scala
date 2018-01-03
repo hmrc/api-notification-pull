0a40692f5865959efc967ef1ee6655ad778afece
Stefano Sgorlon <stefano.sgorlon@digital.hmrc.gov.uk>
2018-01-03 13:23:24 +0000

    API-2934: adding connector and service layer

---
+++ app/uk/gov/hmrc/apinotificationpull/connectors/ApiNotificationQueueConnector.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.apinotificationpull.connectors
+
+import javax.inject.Inject
+
+import uk.gov.hmrc.apinotificationpull.model.Notifications
+import uk.gov.hmrc.apinotificationpull.config.ServiceConfiguration
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.play.bootstrap.http.HttpClient
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class ApiNotificationQueueConnector @Inject()(config: ServiceConfiguration, http: HttpClient) {
+
+  private lazy val serviceBaseUrl: String = config.baseUrl("api-notification-queue")
+
+  private val CLIENT_ID_HEADER_NAME = "X-Client-ID"
+
+  private def addHeaders(clientId: String)(implicit hc: HeaderCarrier): HeaderCarrier = {
+    hc.withExtraHeaders(CLIENT_ID_HEADER_NAME -> clientId)
+  }
+
+  def getQueueNotifications(clientId: String)(implicit hc: HeaderCarrier): Future[Notifications] = {
+    http.GET[Notifications](s"$serviceBaseUrl/notifications") (rds = implicitly, hc = addHeaders(clientId), ec = implicitly)
+  }
+
+}
