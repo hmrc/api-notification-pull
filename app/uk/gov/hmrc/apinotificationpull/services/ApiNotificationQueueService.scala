d84c8355f6c36205e8395b6f9ad2c1e68d2de561
Stew Abel <stew@grupettosoftware.com>
2018-01-08 15:38:46 +0000

    cleanup

--- app/uk/gov/hmrc/apinotificationpull/services/ApiNotificationQueueService.scala
+++ app/uk/gov/hmrc/apinotificationpull/services/ApiNotificationQueueService.scala
@@ -34,9 +34,7 @@ class ApiNotificationQueueService @Inject()(apiNotificationQueueConnector: ApiNo
   def getAndRemoveNotification(notificationId: String)(implicit hc: HeaderCarrier): Future[Option[Notification]] = {
     val notification = apiNotificationQueueConnector.getById(notificationId)
 
-    notification.map(n => {
-      removeFromQueue(n)
-    })
+    notification.map(n => removeFromQueue(n))
 
     notification
   }
