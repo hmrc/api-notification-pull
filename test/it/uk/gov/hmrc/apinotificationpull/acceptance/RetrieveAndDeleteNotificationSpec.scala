224925f714ae285b8f3839f273c95f18ebb2dd6c
Stefano Sgorlon <stefano.sgorlon@digital.hmrc.gov.uk>
2018-01-08 15:57:20 +0000

    API-2932: removing code from this PR

--- test/it/uk/gov/hmrc/apinotificationpull/acceptance/RetrieveAndDeleteNotificationSpec.scala
+++ test/it/uk/gov/hmrc/apinotificationpull/acceptance/RetrieveAndDeleteNotificationSpec.scala
@@ -26,9 +26,9 @@ import play.api.test.Helpers._
 
 class RetrieveAndDeleteNotificationSpec extends FeatureSpec with GivenWhenThen with Matchers with GuiceOneAppPerTest {
 
-  feature("Retrieve(DELETE) a single message from the API Notification Pull service") {
+  feature("Retrieve a single message from the Pull Service (DELETE)") {
     info("As a 3rd Party")
-    info("I want to successfully retrieve a notification waiting for me")
+    info("I want to successfully receive any notifications waiting for me")
     info("So that I can progress my original declaration submission")
 
     val notificationId = UUID.randomUUID()
@@ -37,37 +37,37 @@ class RetrieveAndDeleteNotificationSpec extends FeatureSpec with GivenWhenThen w
     val validRequest = FakeRequest("DELETE", s"/$notificationId").
       withHeaders(ACCEPT -> "application/vnd.hmrc.1.0+xml", xClientId -> clientId)
 
-    scenario("3rd party provides the notification Id, but there are no notifications available in the system.") {
-      Given("A message has already been retrieved using the correct NotificationID")
+    scenario("3rd party provides NotificationID but No message available/Matching NotificationID") {
+      Given("a message has already been retrieved using the correct NotificationID")
 
-      When("You make another call using the same notification Id")
+      When("you make another call using the same MessageID")
       val result = route(app = app, validRequest).value
 
-      Then("You will receive a 404 error response")
+      Then("you will receive a 404 error response")
       status(result) shouldBe NOT_FOUND
       contentAsString(result) shouldBe ""
     }
 
     scenario("Invalid Accept Header") {
-      Given("You provide an invalid or missing Accept Header ")
+      Given("you provide an invalid or missing Accept Header ")
       val request = validRequest.copyFakeRequest(headers = validRequest.headers.remove(ACCEPT))
 
-      When("You call make the 'DELETE' call, with a notification Id, to the api-notification-pull service")
+      When("you call make the 'DELETE' with a NotificationID call to the api-notification-pull service")
       val result = route(app = app, request).value
 
-      Then("You will be returned a 406 error response")
+      Then("you will be returned a 406 error response")
       status(result) shouldBe NOT_ACCEPTABLE
       contentAsString(result) shouldBe ""
     }
 
-    scenario("Missing X-Client-Id Header") {
-      Given("The platform does not inject a X-Client-Id Header")
+    scenario("missing X-Client-Id Header") {
+      Given("the platform does not inject a X-Client-Id Header")
       val request = validRequest.copyFakeRequest(headers = validRequest.headers.remove(xClientId))
 
-      When("You call make the 'DELETE' call, with a notification Id, to the api-notification-pull service ")
+      When("you call make the 'DELETE' with a NotificationID call to the api-notification-pull service ")
       val result = route(app = app, request).value
 
-      Then("You will be returned a 500 error response")
+      Then("you will be returned a 500 error response")
       status(result) shouldBe INTERNAL_SERVER_ERROR
       contentAsString(result) shouldBe ""
     }
