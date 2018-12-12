090ce9246bea8650e07057cc2c21ebca64fdc92f
Paul Blake <paul.blake@digital.hmrc.gov.uk>
2018-12-12 11:58:57 +0000

    CDD-1990: first cut

---
+++ app/uk/gov/hmrc/apinotificationpull/controllers/EnhancedReadController.scala
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.apinotificationpull.controllers
+
+import javax.inject.{Inject, Singleton}
+import play.api.mvc.{Action, AnyContent, Request, Result}
+import uk.gov.hmrc.apinotificationpull.model.XmlErrorResponse
+import uk.gov.hmrc.apinotificationpull.presenters.NotificationPresenter
+import uk.gov.hmrc.apinotificationpull.services.EnhancedApiNotificationQueueService
+import uk.gov.hmrc.apinotificationpull.util.XmlBuilder
+import uk.gov.hmrc.apinotificationpull.validators.HeaderValidator
+import uk.gov.hmrc.customs.api.common.logging.CdsLogger
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.play.bootstrap.controller.BaseController
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+@Singleton
+class EnhancedReadController @Inject()(enhancedApiNotificationQueueService: EnhancedApiNotificationQueueService,
+                                       headerValidator: HeaderValidator,
+                                       notificationPresenter: NotificationPresenter,
+                                       xmlBuilder: XmlBuilder,
+                                       logger: CdsLogger) extends BaseController {
+
+  implicit val hc: HeaderCarrier = HeaderCarrier()
+
+  private val X_CLIENT_ID_HEADER_NAME = "X-Client-ID"
+
+  private def recovery: PartialFunction[Throwable, Result] = {
+    case e =>
+      logger.error(s"An unexpected error occurred: ${e.getMessage}", e)
+      InternalServerError(XmlErrorResponse("An unexpected error occurred"))
+  }
+
+  def read(notificationId: String): Action[AnyContent] =
+    (headerValidator.validateAcceptHeader andThen headerValidator.validateXClientIdHeader).async { implicit request =>
+
+    implicit val hc: HeaderCarrier = buildHeaderCarrier(request)
+
+    enhancedApiNotificationQueueService.getReadNotification(notificationId)
+      .map(notificationPresenter.present)
+      .recover(recovery)
+  }
+
+  private def buildHeaderCarrier(request: Request[AnyContent] ): HeaderCarrier = {
+    request.headers.get(X_CLIENT_ID_HEADER_NAME) match {
+      case Some(clientId: String) => hc.withExtraHeaders(X_CLIENT_ID_HEADER_NAME -> clientId)
+      case _ =>
+        // It should never happen
+        logger.warn(s"Header $X_CLIENT_ID_HEADER_NAME not found in the request.")
+        hc
+    }
+  }
+
+}
