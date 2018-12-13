9288804f439f1b2c1ff01e1e9fde482815919c8a
Rashad Mughal <rash.m2k@gmail.com>
2018-12-13 10:56:36 +0000

    CDD-1652: Final commit.

--- app/uk/gov/hmrc/apinotificationpull/controllers/EnhancedNotificationsController.scala
+++ app/uk/gov/hmrc/apinotificationpull/controllers/EnhancedNotificationsController.scala
@@ -16,15 +16,17 @@
 
 package uk.gov.hmrc.apinotificationpull.controllers
 
+import akka.util.ByteString
 import javax.inject.{Inject, Singleton}
-import play.api.mvc.{Action, AnyContent, Request, Result}
-import uk.gov.hmrc.apinotificationpull.model.XmlErrorResponse
-import uk.gov.hmrc.apinotificationpull.presenters.EnhancedNotificationPresenter
+import play.api.http.HttpEntity
+import play.api.mvc._
+import uk.gov.hmrc.apinotificationpull.model.Notification
 import uk.gov.hmrc.apinotificationpull.services.EnhancedApiNotificationQueueService
 import uk.gov.hmrc.apinotificationpull.util.XmlBuilder
 import uk.gov.hmrc.apinotificationpull.validators.HeaderValidator
+import uk.gov.hmrc.customs.api.common.controllers.ErrorResponse.{ErrorInternalServerError, ErrorNotFound, errorBadRequest}
 import uk.gov.hmrc.customs.api.common.logging.CdsLogger
-import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.http.{BadRequestException, HeaderCarrier, HttpException, NotFoundException}
 import uk.gov.hmrc.play.bootstrap.controller.BaseController
 
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -32,7 +34,6 @@ import scala.concurrent.ExecutionContext.Implicits.global
 @Singleton
 class EnhancedNotificationsController @Inject()(enhancedApiNotificationQueueService: EnhancedApiNotificationQueueService,
                                                 headerValidator: HeaderValidator,
-                                                enhancedNotificationPresenter: EnhancedNotificationPresenter,
                                                 xmlBuilder: XmlBuilder,
                                                 logger: CdsLogger) extends BaseController {
 
@@ -43,17 +44,35 @@ class EnhancedNotificationsController @Inject()(enhancedApiNotificationQueueServ
   private def recovery: PartialFunction[Throwable, Result] = {
     case e =>
       logger.error(s"An unexpected error occurred: ${e.getMessage}", e)
-      InternalServerError(XmlErrorResponse("An unexpected error occurred"))
+      ErrorInternalServerError.XmlResult
   }
 
   def unread(notificationId: String): Action[AnyContent] =
     (headerValidator.validateAcceptHeader andThen headerValidator.validateXClientIdHeader).async { implicit request =>
 
     implicit val hc: HeaderCarrier = buildHeaderCarrier(request)
-
       enhancedApiNotificationQueueService.getUnreadNotificationById(notificationId)
-      .map(enhancedNotificationPresenter.present)
+      .map(r => present(r, notificationId))
       .recover(recovery)
+  }
+
+  private def present(result: Either[HttpException, Notification], notificationId: String): Result = {
+    result match {
+      case Right(n) => Result(
+        header = ResponseHeader(OK),
+        body = HttpEntity.Strict(ByteString(n.payload), n.headers.get(CONTENT_TYPE)))
+        .withHeaders(n.headers.toSeq: _*)
+
+      case Left(nfe: NotFoundException) =>
+        logger.info(s"Notification not found for id: $notificationId", nfe)
+        ErrorNotFound.XmlResult
+      case Left(bre: BadRequestException) =>
+        logger.info(s"Notification has already been read for id: $notificationId", bre)
+        errorBadRequest("Notification has been read").XmlResult
+      case Left(e) =>
+        logger.error(s"Internal server error for notification id: $notificationId", e)
+        ErrorInternalServerError.XmlResult
+    }
   }
 
   private def buildHeaderCarrier(request: Request[AnyContent] ): HeaderCarrier = {
