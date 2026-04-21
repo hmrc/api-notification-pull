7defbf596ac7d5b6a8e457d20957ea01e214aa9a
AdamCoops <adam.cooper2@digital.hmrc.gov.uk>
2026-04-21 09:49:27 +0100

    DCWL-4427-update versions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 object AppDependencies {
 
   val playVersion = "play-30"
-  val bootstrap = "10.6.0"
+  val bootstrap = "10.7.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                   %% s"bootstrap-backend-$playVersion"   % bootstrap
